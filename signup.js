928c0be2cadc24451cc2707de1bde7fe8ec21cac
Ganesh Revanth <ganeshrevanthvasam@gmail.com>
2026-06-16 12:39:15 +0530

    Dashboard.html added & signup.js and firebase.js configured.

--- signup.js
+++ signup.js
@@ -1,19 +1,40 @@
-import { auth, db, createUserWithEmailAndPassword } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-const signupBtn = document.getElementById('signup-btn');
+const form = document.getElementById("signup-form");
 
-signupBtn.addEventListener('click', (e) => {
-    e.preventDefault(); // Stop the form from refreshing the page
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-    const email = document.getElementById('signup-email').value;
-    const password = document.getElementById('signup-password').value;
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const email = document.getElementById("signup-email").value.trim();
+    const password = document.getElementById("signup-password").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const gender = document.getElementById("signup-gender").value;
+    const weight = Number(document.getElementById("weight").value);
+    const height = Number(document.getElementById("height").value);
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            alert("Account Created!");
-            window.location.href = "index.html"; // Redirect to dashboard
-        })
-        .catch((error) => {
-            alert(error.message); // Shows if password is too short or email is taken
+    console.log("Signup button clicked");
+    
+    try{
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
+        await setDoc(doc(db, "users", user.uid), {
+            firstName,
+            lastName,
+            email,
+            birthdate,
+            gender,
+            weight,
+            height,
+            createdAt: Date.now()
         });
+
+        window.location.href = "dashboard.html";
+    } catch (error) {
+        alert(error.message);
+    }
 });
